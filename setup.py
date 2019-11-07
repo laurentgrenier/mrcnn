c9f086ca4cd18f24a9a9432582e3577544f462fb
Laurent Grenier <laurent.grenier.pro@gmail.com>
2019-11-07 11:09:10 +0100

    creation

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='mrcnn',
+      version='0.1.0',
+      description='My version derivated from mattersport',
+      url='https://github.com/laurentgrenier/mrcnn',
+      author='Laurent Grenier',
+      author_email='laurent@mooke.io',
+      license='MIT',
+      packages=['mrcnn'],
+      install_requires=[
+            'numpy==1.17.3',
+            'pandas==0.25.3',
+            'matplotlib==3.1.1',
+            'scikit-image==0.16.2',
+            'imgaug==0.2.6',
+            'keras==2.2.4',
+            'tensorflow-gpu==1.12.0',
+            'albumentations==0.3.3',
+            'IPython[all]'
+      ],
+      zip_safe=False)
+
